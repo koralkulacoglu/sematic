0dcf68e955a52dfca225ed8a11cc71489006fd4c
Koral Kulacoglu <koralkulacoglu168@bitgo.com>
2025-09-22 00:50:31 -0400

    update branding and remove placeholders

--- frontend/src/App.test.js
+++ frontend/src/App.test.js
@@ -1,8 +1,13 @@
 import { render, screen } from '@testing-library/react';
+import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+test('renders Sematic application', () => {
+  render(
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  );
+  const sematicElement = screen.getByText(/sematic/i);
+  expect(sematicElement).toBeInTheDocument();
 });
